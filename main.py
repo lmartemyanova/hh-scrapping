463f8104d7ff52549743d24c7db83fede7668de1
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-06-17 15:02:14 +0300

    parsing excluding city

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import fake_useragent
 
 
-def get_html(text):
+def get_links(text):
     fake_headers = fake_useragent.UserAgent()
     headers = {'user-agent': fake_headers.random}
     response = requests.get(
@@ -22,7 +22,7 @@ def get_html(text):
             pages_count = int(soup.find('div', class_='pager').find_all('span', recursive=False)[-1].find('a').find('span').text)
         except Exception:
             return
-        vacancies = {}
+        links = []
         for page in range(pages_count):
             response = requests.get(
                 url=f'https://hh.ru/search/vacancy?area=1&area=2&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&text={text}&customDomain=1&page={page}',
@@ -32,17 +32,53 @@ def get_html(text):
                 continue
             try:
                 soup = BeautifulSoup(response.content, 'lxml')
-                # for name in soup.find_all(''):
-                #     print()
-                # for description in soup.find_all(''):
-                #     if 'django' in description.lower() or 'flask' in description.lower():
-                #
                 for link in soup.find_all('a', class_='serp-item__title'):
                     link = f'{link["href"]}'
-                    print(link)
+                    links.append(link)
+                    # print(link)
             except Exception as e:
                 print(f'{e}')
             time.sleep(1)
+    return links
+
+
+def get_vacancies(links):
+    fake_user = fake_useragent.UserAgent()
+    headers = fake_user.random
+    vacancies = []
+    for link in links:
+        response = requests.get(
+            url=link,
+            headers={'user-agent': headers}
+        )
+        if response.status_code != 200:
+            print(f"Ошибка {response.status_code}")
+            continue
+        try:
+            soup = BeautifulSoup(response.content, 'lxml')
+            description = soup.find('div', class_='g-user-content').text
+            if 'django' not in description.lower() \
+                    and 'flask' not in description.lower():
+                continue
+            position = soup.find('div', class_='vacancy-title').find('h1', class_='bloko-header-section-1').text
+            company = soup.find('div', class_='vacancy-company-details').find('span', class_='vacancy-company-name').text
+            salary = soup.find('div', class_='vacancy-title').find('span', class_='bloko-header-section-2 bloko-header-section-2_lite').text
+            if not salary:
+                salary = "не указана"
+            city = soup.find('div', class_='vacancy-company-redesigned').find('vacancy-view-location').text
+            if not city:
+                city = "не указан"
+            vacancy = {
+                'link': link,
+                'position': position,
+                'company': company,
+                'salary': salary,
+                'city': city
+            }
+            vacancies.append(vacancy)
+        except Exception as e:
+            print(f'{e}')
+        time.sleep(1)
     return vacancies
 
 
@@ -53,4 +89,6 @@ def write_json(vacancies):
 
 
 if __name__ == "__main__":
-    get_html('python')
+    # links = get_links('python')
+    print(get_vacancies(links=['https://hh.ru/vacancy/81639879?from=vacancy_search_list&query=python']))
+    # print(get_vacancies(links=links))
